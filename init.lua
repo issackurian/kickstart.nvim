2fb03f2650d04c034aa2a6599d0a3719e54017ef
Issac Kurian <issac@issac.com>
2025-03-21 20:38:53 -0400

    Add LSPs

--- init.lua
+++ init.lua
@@ -680,9 +680,54 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        basedpyright = {},
+        -- pylsp = {
+        --   plugins = {
+        --     -- pycodestyle = {
+        --     --   ignore = {'W391'},
+        --     --   maxLineLength = 120
+        --     -- },
+        --     -- flake8 = {
+        --     --   ignore = {'W391'},
+        --     --   maxLineLength = 120
+        --     -- },
+        --     rope_autoimport = {
+        --       enabled = true,
+        --     },
+        --     -- rope_completion = {
+        --     --   enabled = true,
+        --     --   eager = true
+        --     -- },
+        --     pylsp_mypy = {
+        --       enabled = true,
+        --       live_mode = true,
+        --     },
+        --     ruff = {
+        --       enabled = true, -- Enable the plugin
+        --       -- executable = "<path-to-ruff-bin>",  -- Custom path to ruff
+        --       -- config = "<path_to_custom_ruff_toml>",  -- Custom config for ruff to use
+        --       -- extendSelect = { "I" },  -- Rules that are additionally used by ruff
+        --       -- extendIgnore = { "C90" },  -- Rules that are additionally ignored by ruff
+        --       format = { 'I' }, -- Rules that are marked as fixable by ruff that should be fixed when running textDocument/formatting
+        --       -- severities = { ["D212"] = "I" },  -- Optional table of rules where a custom severity is desired
+        --       unsafeFixes = false, -- Whether or not to offer unsafe fixes as code actions. Ignored with the "Fix All" action
+        --
+        --       -- Rules that are ignored when a pyproject.toml or ruff.toml is present:
+        --       lineLength = 120, -- Line length to pass to ruff checking and formatting
+        --       -- exclude = { "__about__.py" },  -- Files to be excluded by ruff checking
+        --       -- select = { "F" },  -- Rules to be enabled by ruff
+        --       -- ignore = { "D210" },  -- Rules to be ignored by ruff
+        --       -- perFileIgnores = { ["__init__.py"] = "CPY001" },  -- Rules that should be ignored for specific files
+        --       preview = false, -- Whether to enable the preview style linting and formatting.
+        --       targetVersion = 'py38', -- The minimum python version to target (applies for both linting and formatting).
+        --     },
+        --   },
+        -- },
+        -- metals = {},
+        rust_analyzer = {},
+        terraformls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
